b4e3f13698d161172bcb1799e7728dcac93d2226
inifikry <48782289+inifikry@users.noreply.github.com>
2024-10-24 15:59:55 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/inifikry/onedrive-vercel-index-inifikry" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by inifikry.',
+    'Powered by <a href="https://ovi.swo.moe" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by inifikry.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -52,8 +52,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/inifikry/onedrive-vercel-index-inifikry',
+      name: 'Discord',
+      link: 'https://discord.com/users/456842882465398785',
     },
   ],
 
